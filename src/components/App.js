c21208b5be8f88c89bb2abb8de0715805abfefc0
zhanfeng.hzf <admin@xhou.net>
2017-09-23 23:47:01 +0800

    update demo

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,20 @@ class App extends Component {
     return (
       <div className="app">
         <div className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h2>Welcome to Dawn & React</h2>
+          <h2 className="app-welcome">Welcome to Dawn</h2>
         </div>
-        <p className="app-intro">
-          To get started, edit <code>src/components/App.js</code> and save to reload.
-        </p>
+        <div className="app-intro">
+          <img src={logo} className="app-logo" alt="logo" />
+          <div>
+            To get started, edit <code>src/components/App.js</code> and save to reload.
+          </div>
+        </div>
+        <ul className="app-links">
+          <li><a href="https://alibaba.github.io/dawn/">Homepage</a></li>
+          <li><a href="https://alibaba.github.io/dawn/docs">Documents</a></li>
+          <li><a href="https://github.com/alibaba/dawn">Git Repo</a></li>
+          <li><a href="https://github.com/alibaba/dawn/issues">Issues</a></li>
+        </ul>
       </div>
     );
   }
